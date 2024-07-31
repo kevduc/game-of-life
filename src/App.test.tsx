5265862dc67be499b270acf477d5806a0e1f7ba8
kevduc <64347790+kevduc@users.noreply.github.com>
2024-07-31 05:14:05 +0100

    dist

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import { describe, test } from 'vitest';
-import { App } from './App';
-
-describe('App', () => {
-  test('it renders the App component', () => {
-    render(<App />);
-  });
-});
--- src/utils/sum.ts
+++
@@ -1,1 +0,0 @@
-export const sum = (numbers: number[]) => numbers.reduce((sum, value) => sum + value, 0);
